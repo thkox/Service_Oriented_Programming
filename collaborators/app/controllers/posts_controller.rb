46adf63487a7395f2fd6bd2e857ce6a365c05592
Theodoros Koxanoglou <theokoxan@gmail.com>
2024-02-08 20:40:44 +0000

    Create a UI to create new posts - Inside the PostsController:   define new and create actions   define a post_params method   define a before_action filter - Inside the ApplicationController:   define a redirect_if_not_signed_in method   define a redirect_if_signed_in method - Create a new template for posts

--- collaborators/app/controllers/posts_controller.rb
+++ collaborators/app/controllers/posts_controller.rb
@@ -1,4 +1,8 @@
 class PostsController < ApplicationController
+
+  before_action :redirect_if_not_signed_in, only: [:new]
+
+
     def show
         @post = Post.find(params[:id])
     end
@@ -14,6 +18,30 @@ class PostsController < ApplicationController
         posts_for_branch(params[:action])
     end
 
+    def new
+      @branch = params[:branch]
+      @categories = Category.where(branch: @branch)
+      @post = Post.new
+    end
+
+    def get_posts
+      PostsForBranchService.new({
+        search: params[:search],
+        category: params[:category],
+        branch: params[:action]
+      }).call
+    end
+
+  
+    def create
+      @post = Post.new(post_params)
+      if @post.save 
+        redirect_to post_path(@post) 
+      else
+        redirect_to root_path
+      end
+    end
+
     private
 
     def posts_for_branch(branch)
@@ -25,26 +53,9 @@ class PostsController < ApplicationController
         format.js { render partial: 'posts/posts_pagination_page' }
       end
     end
-
-    def get_posts
-        Post.limit(30)
-    end
-  
-    def get_posts
-        branch = params[:action]
-        search = params[:search]
-        category = params[:category]
-      
-        if category.blank? && search.blank?
-          posts = Post.by_branch(branch).all
-        elsif category.blank? && search.present?
-          posts = Post.by_branch(branch).search(search)
-        elsif category.present? && search.blank?
-          posts = Post.by_category(branch, category)
-        elsif category.present? && search.present?
-          posts = Post.by_category(branch, category).search(search)
-        else
-        end
-    end
     
+    def post_params
+      params.require(:post).permit(:content, :title, :category_id)
+                           .merge(user_id: current_user.id)
+    end
 end
