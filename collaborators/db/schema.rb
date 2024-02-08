d87bdc6f1f277141179d143247d6dc99a29985b1
Apostolis2002 <apostolissiabanis@hotmail.com>
2024-02-08 02:04:41 +0200

    - Generate Post and Category models. - Create associations between User, Post and Catergory models. - Create categories and posts database tables.

--- collaborators/db/schema.rb
+++ collaborators/db/schema.rb
@@ -10,9 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_02_07_160316) do
+ActiveRecord::Schema[7.1].define(version: 2024_02_07_235024) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.string "branch"
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
