cae2ea3ff46e551cfb9e1623f6024c5621dd2fea
Apostolis2002 <apostolissiabanis@hotmail.com>
2024-02-08 02:47:55 +0200

    - Add faker gem - Inside the seeds.rb file create methods to generate User, Category and Post records inside the development database

--- collaborators/db/schema.rb
+++ collaborators/db/schema.rb
@@ -20,8 +20,14 @@ ActiveRecord::Schema[7.1].define(version: 2024_02_07_235024) do
   end
 
   create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.text "content"
+    t.bigint "category_id"
+    t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["category_id"], name: "index_posts_on_category_id"
+    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
