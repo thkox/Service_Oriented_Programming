215f1fc70aabf15524d10e7c0c09b64a76f4463a
Apostolis2002 <apostolissiabanis@hotmail.com>
2024-02-11 16:37:00 +0200

    Create a link on the navigation bar to open a group conversation

--- collaborators/spec/helpers/shared/conversations_helper_spec.rb
+++ collaborators/spec/helpers/shared/conversations_helper_spec.rb
@@ -41,4 +41,30 @@ RSpec.describe Shared::ConversationsHelper, :type => :helper do
     end
   end
 
+  context '#group_conv_seen_status' do
+    it 'returns unseen-conv status' do
+      conversation = create(:group_conversation, messages: [create(:group_message)])
+      current_user = create(:user)
+      allow(helper).to receive(:current_user).and_return(current_user)
+      expect(helper.group_conv_seen_status(conversation, current_user)).to eq 'unseen-conv'
+    end
+
+    it 'returns an empty string' do
+      user = nil
+      conversation = create(:group_conversation, messages: [create(:group_message)])
+      allow(helper).to receive(:current_user).and_return(user)
+      expect(helper.group_conv_seen_status(conversation, user)).to eq ''
+    end
+
+    it 'returns an empty string' do
+      user = create(:user)
+      conversation = create(:group_conversation, messages: [])
+      message = build(:group_message, conversation_id: conversation.id)
+      message.seen_by << user.id
+      message.save
+      conversation.messages << message
+      allow(helper).to receive(:current_user).and_return(user)
+      expect(helper.group_conv_seen_status(conversation, user)).to eq ''
+    end
+  end
 end
